e527330667cd07a7a555c2fd1bfdf715e2c1b9aa
George <george@gcoope.co.uk>
2015-09-14 12:32:28 +0100

    Added basic lobby

---
+++ Assets/Scripts/Networking/LobbyController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour {
+
+	public NetworkManager manager;
+	public InputField nameBox;
+
+	void Awake() {
+		manager = GetComponent<NetworkManager>();
+	}
+
+	public void HostLan() {
+		manager.StartHost();
+	}
+
+	public void JoinAsClient() {
+		manager.StartClient();
+	}
+
+}
