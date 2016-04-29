8a0644075600048bbc671551965421618d312cc1
George <george@gcoope.co.uk>
2016-04-29 09:51:33 +0100

    Added video splashscreen - might need fade in/out functionality doing still

---
+++ Assets/AutoPlayMovie.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AutoPlayMovie : MonoBehaviour {
+
+	Renderer rend;
+	MovieTexture mTexture;
+	AudioSource audioSrc;
+
+	bool vidStarted = false;
+	bool vidEnded = false;
+
+	float videoDuration;
+
+	 void Awake() {
+		rend = GetComponent<Renderer>();
+		mTexture = (MovieTexture)rend.material.mainTexture;
+		videoDuration = mTexture.duration;
+		audioSrc = GetComponent<AudioSource>();
+		audioSrc.clip = mTexture.audioClip;
+	}
+
+	 void Start () 	{		
+		if(!mTexture.isPlaying) {
+			StartCoroutine("ChangeAtEnd");
+			mTexture.Play();
+			audioSrc.Play();
+		}
+	}
+
+	void Update() {
+		float quadHeight = Camera.main.orthographicSize * 2.0f;
+		float quadWidth = quadHeight * Screen.width / Screen.height;
+		transform.localScale = new Vector3(quadWidth, quadHeight, 1);
+
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape)) {
+			StopCoroutine("ChangeAtEnd");
+			GoToStartScene();
+		}
+	}
+
+	IEnumerator ChangeAtEnd() {
+		yield return new WaitForSeconds(videoDuration-0.5f);
+		GoToStartScene();
+	}
+
+	private void GoToStartScene() {
+		SceneManager.LoadScene(1);
+
+	}
+
+}
--- Assets/Scripts/Gameplay/Spawning/SpawnPoint.cs
+++ Assets/Scripts/Gameplay/Spawning/SpawnPoint.cs
@@ -10,3 +10,4 @@ public class SpawnPoint : MonoBehaviour {
 		return true;
 	}
 }
+	
